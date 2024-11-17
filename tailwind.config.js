830b5235a536448ce62ed391c0326310ec234562
rono1579 <135011784+rono1579@users.noreply.github.com>
2024-11-17 08:39:20 -0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Paths to all template files
+  ],
+  theme: {
+    extend: {}, // Extend the default theme here if needed
+  },
+  plugins: [], // Add any Tailwind CSS plugins here
+};
