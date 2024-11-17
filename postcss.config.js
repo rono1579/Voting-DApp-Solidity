9beef534b26d3682f7b71f6749fb29dadaca1308
rono1579 <135011784+rono1579@users.noreply.github.com>
2024-11-17 08:38:22 -0800

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {}, // Tailwind CSS for utility-first styling
+    autoprefixer: {}, // Autoprefixer for adding vendor prefixes
+  },
+};
